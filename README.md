910b53a7f00d3451b778f54c07517094b1cfbc20
Matheus Pavanelli <mspavanelli@gmail.com>
2015-09-23 00:53:35 -0300

    Revert "W"
    This reverts commit 35d5f11651c59fba6bb388746692c0230da772fa.

---
+++ README.md
@@ -0,0 +1,53 @@
+# Aplicação para servir de base a novas ferramentas HTML5/Javascript/CSS
+
+Projeto com ferramentas e estrutura básicas para a criação em HTML5/Javascript/CSS
+
+## Dependências:
+- NodeJS
+- NPM (node package manager, na instalação do NodeJS geralmente o NPM já vem instalado junto.)
+- O bin do node estar dentro do path
+
+## Rodando através do servidor embutido (dispensa instalação do Apache):
+- executar no shell:
+    - node basic_server.js
+
+## Organização
+
+A organização padrão dos diretórios é a seguinte:
+
+- html
+- css
+	- min
+- js
+	- min
+- img (para as imagens)
+- lib (recursos externos)
+
+### Pasta dist
+
+Há uma pasta _dist_ que contêm 3 arquivos:
+
+- index.html
+- main.min.css
+- main.min.js
+
+Neles está agrupado em forma minificada todo o código gerado.
+
+## Automatização e Comandos
+Ao executar o comando `gulp` as seguintes operações são realizadas:
+	
+* Qualquer modificação que for salva nos arquivos será automaticamente atualizada no navegador
+
+Há também comandos mais específicos para algumas tarefas tarefas
+
+`gulp`
+:Inicia o servidor já com autocarregamento da página
+
+`gulp organize`
+: Cria diretórios conforme especificado
+
+`min`
+: Minifica as folhas de estilo e funções javascript
+
+`gulp angularJS`
+: Cria diretórios conforme especificado
---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var connect = require('gulp-connect');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var minifyCSS = require('gulp-minify-css');
+
+var app = 'Teste/'; // endereço do projeto
+
+/* Cria os diretórios */
+gulp.task('dir', function() {
+	gulp.src(app)
+	.pipe(gulp.dest(app + 'html/'))
+	.pipe(clean())
+	.pipe(gulp.dest(app + 'css/min/'))
+	.pipe(clean())
+	.pipe(gulp.dest(app + 'js/min/'))
+	.pipe(clean())
+	.pipe(gulp.dest(app + 'img/'))
+	.pipe(clean())
+	.pipe(gulp.dest(app + 'lib/'))
+	.pipe(clean());
+});
+
+gulp.task('server-reload', function() {
+	browserSync({
+		logPrefix: 'IME',
+		server: [app]
+	});
+	gulp.watch([app + '**'], ['min']);
+	gulp.watch([app + '**/*.*'], reload);
+});
+
+/* Move os arquivos para as pastas correspondentes*/
+gulp.task('moveFiles', function() {
+	gulp.src(app + '*.html')
+	.pipe(gulp.dest(app + 'html/'));
+	
+	gulp.src(app + '*.css')
+	.pipe(gulp.dest(app + 'css/'))
+	.pipe(minifyCSS())
+	.pipe(gulp.dest(app + 'css/min/'));
+	
+	gulp.src(app + '*.js')
+	.pipe(gulp.dest(app + 'js/'))
+	.pipe(uglify())
+	.pipe(gulp.dest(app + 'js/min'));
+	
+	gulp.src(app + '*.jpg')
+	.pipe(gulp.dest(app + 'img/'));
+
+	gulp.src(app + '*.png')
+	.pipe(gulp.dest(app + 'img/'));
+});
+
+/*  Minifica o CSS */
+gulp.task('css-min', function() {
+	gulp.src(app + 'css/*.css')
+	.pipe(minifyCSS())
+	.pipe(gulp.dest(app + 'css/min/'));
+});
+
+/*  Minifica o JS */
+gulp.task('js-min', function() {
+	gulp.src(app + 'js/*.js')
+	.pipe(uglify())
+	.pipe(gulp.dest(app + 'js/min'));
+});
+
+/* Minifica CSS e JS */
+gulp.task('min', ['css-min', 'js-min']);
+
+/* Limpa os arquivos */
+gulp.task('limpe', function() {
+	gulp.src(app + '*.html').pipe(clean());
+	gulp.src(app + '*.css').pipe(clean());
+	// gulp.src(app + '*.js').pipe(clean());
+	gulp.src(app + '*.jpg').pipe(clean());
+	gulp.src(app + '*.png').pipe(clean());
+});
+
+/* Organização Geral dos Arquivos */
+gulp.task('organize', ['moveFiles', 'limpe']);
+
+gulp.task('angularJS', function() {
+	var jsFiles = gulp.src(app + 'js/');
+
+	if ( jsFiles.match(/(js)/) ) {
+		console.log( "js file" );
+	}
+})
+
+gulp.task('default', ['min', 'server-reload']);
